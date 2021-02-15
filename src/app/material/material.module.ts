9a8491cd7390742d391e55cec2306df0ec660535
markwartman1 <markwartman1@gmail.com>
2021-02-15 11:33:30 -0600

    Menu & SubMenu

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatMenuModule } from "@angular/material/menu";
 
 const material = [
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatMenuModule
 ];
 
 @NgModule({
