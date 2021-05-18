246132b2638516f0fb7a4e1f12852ffe741154fc
markwartman1 <markwartman1@gmail.com>
2021-05-18 09:18:10 -0500

    datepicker date range

--- src/app/components/datepicker/datepicker.component.ts
+++ src/app/components/datepicker/datepicker.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatepickerComponent implements OnInit {
 
+  // DATE PARAMS
+  minDate = new Date(2016, 0, 1);
+  maxDate = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
